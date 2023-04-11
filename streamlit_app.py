a99cdd7f7479562151d5910661165a91fe5b6c61
lokesh2191 <130332572+lokesh2191@users.noreply.github.com>
2023-04-10 18:01:58 -1000

    Update streamlit_app.py
    adding control flow changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import snowflake.connector
+import requests
+from URllib.error import URLError
+
 streamlit.title('My Mom''s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -17,15 +21,14 @@ streamlit.header('FruityVice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize semi-structured data 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # use streamlit dataframe to display normalized data
 streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
