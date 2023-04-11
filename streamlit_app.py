e51d8b03faf69115f5898730237233f7ff714f60
lokesh2191 <130332572+lokesh2191@users.noreply.github.com>
2023-04-10 15:25:54 -1000

    Update streamlit_app.py
    implementing streamlit variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('FruityVice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
