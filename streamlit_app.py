ca32716abce530dea653d4c789055b1216ffa032
lokesh2191 <130332572+lokesh2191@users.noreply.github.com>
2023-04-10 15:18:32 -1000

    Update streamlit_app.py
    redundant data

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # Normalize semi-structured data 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
