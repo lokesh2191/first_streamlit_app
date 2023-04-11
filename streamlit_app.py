ff59e748d751fdf150d409e8aa843a11a0cc0676
lokesh2191 <130332572+lokesh2191@users.noreply.github.com>
2023-04-10 15:14:03 -1000

    Update streamlit_app.py
    summarizing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,19 @@
-import requests
 import streamlit
+import pandas
+streamlit.title('My Mom''s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
