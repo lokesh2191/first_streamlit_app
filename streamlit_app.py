1b527b3bcd54cdbe3e23e8936b2e44a99eb7bb0a
lokesh2191 <130332572+lokesh2191@users.noreply.github.com>
2023-04-10 15:17:38 -1000

    Update streamlit_app.py
    normalizing data

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,9 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+# Normalize semi-structured data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# use streamlit dataframe to display normalized data
+streamlit.dataframe(fruityvice_normalized)
